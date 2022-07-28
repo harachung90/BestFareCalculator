ef0e06cbbad0ab8bf15d5a77cc18dc10d0606f6d
Hara <harachung90@gmail.com>
2022-07-28 20:16:35 +1000

    Second commit. Step 7-9. Complete!

--- src/TransitCalculator.java
+++ src/TransitCalculator.java
@@ -5,34 +5,55 @@ public class TransitCalculator {
     int numDays;
     int numRides;
 
-    double singleRideFare = 2.75;
-    double sevenDayUnlimitedFare = 33.00;
-    double thirtyDayUnlimitedFare = 127.00;
+    String[] fareOptions = {"Pay-per-ride", "7-Day Unlimited", "30-Day Unlimited"};
+    double[] fares = {2.75, 33.00, 127.00};
+
+    public TransitCalculator(int days, int rides) {
+        numDays = days;
+        numRides = rides;
+    }
 
     public double unlimited7Price() {
-        int amount7Pass = numDays / 7;
-        if (numDays % 7 != 0) {
-            amount7Pass += 1;
-        }
 
-        double singleFare = Math.ceil(sevenDayUnlimitedFare * amount7Pass / numRides);
-        return singleFare;
+        double sevenDayPurchases = Math.ceil(numDays / 7.0);
+        double totalCost = Math.ceil(fares[1] * sevenDayPurchases);
+
+        return totalCost / numRides;
+    }
+
+    public double[] getRidePrices() {
+
+        double payPerRidePrice = fares[0];
+        double unlimited7Price = unlimited7Price();
+        double unlimited30Price = fares[2] / numRides;
+
+        double[] prices = {payPerRidePrice, unlimited7Price, unlimited30Price};
+
+        return prices;
+    }
+
+    public String getBestFare() {
+
+        double[] ridePrices = getRidePrices();
+        int theIndex = 0;
+
+        for (int i = 0; i < ridePrices.length; i++) {
+            if (ridePrices[i] < ridePrices[theIndex]) {
+                theIndex = i;
+            }
+
+        }
+        return "You should get " + fareOptions[theIndex] + " option at $" + Math.round(ridePrices[theIndex] * 100.0) / 100.0 + " per ride.";
     }
 
     public static void main(String[] args) {
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int myDays = 5;
+        int myRides = 12;
 
-        System.out.println("How many days are you going to using the transit system?");
-        int numDays = myObj.nextInt();  // Read user input
-        System.out.println("You'll be using the transit system for "+ numDays +" day(s).");  // Output user input
+        TransitCalculator trip1 = new TransitCalculator(myDays, myRides);
 
-        System.out.println("How many rids are you going to take during that time?");
-        int numRides = myObj.nextInt();  // Read user input
-        System.out.println("You'll be using the transit system for " + numRides + " ride(s).");  // Output user input
-        myObj.close();
-
-
+        System.out.println(trip1.getBestFare());
 
     }
 }
